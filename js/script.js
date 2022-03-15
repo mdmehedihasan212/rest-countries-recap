249db0de04491ffe16d6820cb30533a7aadec68f
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-03-15 15:52:54 +0600

    display result add css

--- js/script.js
+++ js/script.js
@@ -8,16 +8,17 @@ const displayData = countries => {
 
     const allCountry = countries.map((country) => getCountry(country))
     const displayId = document.getElementById('countries');
-    displayId.innerHTML = allCountry;
+    displayId.innerHTML = allCountry.join(' ');
 }
 
 
 const getCountry = country => {
-    console.log(country);
     return `
+    <div class="country">
     <h2>${country.name.common}</h2>
-    <h3>${country.continents[0]}</h3>
+    <h3>${country.continents}</h3>
     <img src="${country.flags.png}">
+    </div>
     `;
 }
 fetchApi();
